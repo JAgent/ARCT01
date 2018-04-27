c46e28587f0c8c4780c9c97c258ec1c85a27a6e4
Yamamoto Masao <yamamoto.masa-y@jp.fujitsu.com>
2018-04-27 15:05:10 +0900

    ちょっとスケールはおかしいけどポジトラは動作する

---
+++ Assets/Scripts/PosiTra.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using GoogleARCore;
+
+public class PosiTra : MonoBehaviour {
+
+	private Vector3 origPos;
+
+	void Start () {
+		origPos = gameObject.transform.position;
+	}
+
+	void Update () {
+
+		if (Session.Status == SessionStatus.Tracking)
+		{
+			var pose = Frame.Pose;
+			gameObject.transform.position = pose.position + origPos;
+		}
+	}
+}
